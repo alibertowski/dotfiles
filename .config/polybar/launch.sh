09465a127596c2e5863702819b168acb87fda705
DominoGhost <57733758+DominoGhost@users.noreply.github.com>
2020-11-09 18:27:30 -0500

    Modifications

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,8 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1
-echo "---" | tee -a /tmp/polybar1.log
-polybar example >>/tmp/polybar1.log 2>&1 & disown
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar bar1 >>/tmp/polybar1.log 2>&1 & disown
+polybar bar2 >>/tmp/polybar2.log 2>&1 & disown
 
 echo "Bars launched..."

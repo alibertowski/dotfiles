0cef9173c7491ca3ba5c0dd13fa7bdfeac48d171
DominoGhost <57733758+DominoGhost@users.noreply.github.com>
2021-10-15 13:52:38 -0400

    Created setup script

--- setup-configs.sh
+++ setup-configs.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-echo "Setting Links"
+echo "Copying and setting links for configurations"
+cp -rl ./.config $HOME/
+cp -l ./.bashrc $HOME/.bashrc
+cp -l ./.vimrc $HOME/.vimrc
+cp -l ./.xinitrc $HOME/.xinitrc

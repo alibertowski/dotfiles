4edd483053ce64f87701e61fdf0dfc058d1c8ab2
DominoGhost <57733758+DominoGhost@users.noreply.github.com>
2022-04-26 23:14:28 -0400

    Fixes and additional drive support

--- archlinux-install.sh
+++ archlinux-install.sh
@@ -5,8 +5,21 @@
 
 # TODO:
 #	Encryption
-#	Additional Drives
+#	Fix UEFI
 #	Secure Boot
+#	New bootloader?
+#	Support Wi-Fi - Not urgent
+#	Mount /boot when pacman updates
+#	Test unmounted /boot with UEFI
+
+# TODO Easy:
+#	Improving performance page - https://wiki.archlinux.org/title/Improving_performance
+#	Security page - https://wiki.archlinux.org/title/Security
+#	Sudo permissions in post-install
+#	Pipewire
+#	nowatchdog
+#	Multilib 32bit support
+#	Pacman colors
 
 readonly HOSTNAME="retro"
 readonly TIMEZONE_REGION="America"
@@ -17,25 +30,33 @@ readonly DEBUG="y"
 readonly GPU="other"
 
 # Boot, Root, Swap, EFI, Additonal Drives
-readonly SWAP_DRIVE="/dev/sda"
-readonly SWAP_TYPE="drive" # Can be one of the following: (drive, file, none)
+readonly SWAP_DRIVE="/dev/sdb"
+readonly SWAP_TYPE="drive" # Can be one of the following: (drive or none)
 readonly SWAP_SIZE=("1MiB" "1GiB")
 readonly SWAP_LABEL="Swap"
 readonly SWAP_PARTITION="1"
 
 # Drive, Size, Name, Partition
 readonly ROOT_DRIVE="/dev/sda"
-readonly ROOT_SIZE=("2GiB" "100%")
+readonly ROOT_SIZE=("1GiB" "8GiB")
 readonly ROOT_LABEL="Root"
-readonly ROOT_PARTITION="3"
+readonly ROOT_PARTITION="2"
 
 # Drive or root (Included in root), Size, Name
 readonly BOOT_DRIVE="/dev/sda"
-readonly BOOT_SIZE=("1GiB" "2GiB")
+readonly BOOT_SIZE=("1MiB" "1GiB")
 readonly BOOT_LABEL="Boot"
-readonly BOOT_PARTITION="2"
+readonly BOOT_PARTITION="1"
 
-PackagesNeeded=(base linux linux-firmware sudo pacman-contrib vim ufw grub python python2 man-db man-pages texinfo git polkit)
+# Drive, Size, Name, Partition
+readonly ADDITIONAL_DRIVES=("/dev/sda" "/dev/sdb")
+readonly ADDITIONAL_SIZE1=("8GiB" "1GiB")
+readonly ADDITIONAL_SIZE2=("100%" "100%")
+readonly ADDITIONAL_LABEL=("TestDrive" "OtherDrive")
+readonly ADDITIONAL_PARTITION=("3" "2")
+readonly ADDITIONAL_MOUNTPOINT=("/mnt/root/test" "/mnt/home/alex")
+
+PackagesNeeded=(base linux linux-firmware iptables-nft sudo pacman-contrib vim ufw grub python python2 man-db man-pages texinfo git polkit dhcpcd htop)
 
 function validate_variables() {
 	if [ "$DEBUG" == "y" ]; then
@@ -94,6 +115,12 @@ function partition_drives() {
 		drivesToPart[${#drivesToPart[@]}]=$EFI_DRIVE
 	fi
 
+	for drive in "${ADDITIONAL_DRIVES[@]}"; do
+		if [[ ! "${drivesToPart[*]}" =~ $drive ]]; then
+			drivesToPart[${#drivesToPart[@]}]=$drive
+		fi
+	done
+
 	for drive in "${drivesToPart[@]}"; do
 		local totalPartitions="0"
 		local currentPartition="1"
@@ -120,6 +147,12 @@ function partition_drives() {
 			totalPartitions=$((totalPartitions + 1))
 		fi
 
+		for additionalDrive in "${ADDITIONAL_DRIVES[@]}"; do
+			if [ "$additionalDrive" = "$drive" ]; then
+				totalPartitions=$((totalPartitions + 1))
+			fi
+		done
+
 		while [ $currentPartition -le "$totalPartitions" ]; do
 			# Partitioning any needed drives
 			if [[ "$SWAP_DRIVE" = "$drive" && $SWAP_PARTITION -eq $currentPartition ]]; then
@@ -130,6 +163,9 @@ function partition_drives() {
 				else
 					parted -s "$drive" mkpart primary linux-swap "${SWAP_SIZE[0]}" "${SWAP_SIZE[1]}"
 				fi
+
+				mkswap -L "$SWAP_LABEL" "${SWAP_DRIVE}${SWAP_PARTITION}"
+				swapon "${SWAP_DRIVE}${SWAP_PARTITION}"
 
 				currentPartition=$((currentPartition + 1))
 			fi
@@ -158,9 +194,42 @@ function partition_drives() {
 				fi
 			fi
 
+			for (( i=0; i<${#ADDITIONAL_DRIVES[@]}; i++ )); 
+			do
+				if [[ "${ADDITIONAL_DRIVES[$i]}" = "$drive" && "${ADDITIONAL_PARTITION[$i]}" -eq "$currentPartition" ]]; then
+					if [ "$IS_UEFI" = "true" ]; then
+						parted -s "$drive" mkpart "${ADDITIONAL_LABEL[$i]}" ext4 "${ADDITIONAL_SIZE1[$i]}" "${ADDITIONAL_SIZE2[$i]}"
+					else
+						parted -s "$drive" mkpart primary ext4 "${ADDITIONAL_SIZE1[$i]}" "${ADDITIONAL_SIZE2[$i]}"
+					fi
+
+					# TODO: Change the encrypted name mapping
+					if [ "$ENCRYPT" = "y" ]; then
+						cryptsetup -y -v luksFormat "${ADDITIONAL_DRIVES[$i]}${ADDITIONAL_PARTITION[$i]}"
+						cryptsetup open "${ADDITIONAL_DRIVES[$i]}${ADDITIONAL_PARTITION[$i]}" root
+						mkfs.ext4 -L "${ADDITIONAL_LABEL[$i]}" /dev/mapper/root
+						mkdir -p "${ADDITIONAL_MOUNTPOINT[$i]}"
+						mount /dev/mapper/root "${ADDITIONAL_MOUNTPOINT[$i]}"
+					else # TODO: Fix this bug
+						mkfs.ext4 -L "${ADDITIONAL_LABEL[$i]}" "${ADDITIONAL_DRIVES[$i]}${ADDITIONAL_PARTITION[$i]}"
+						echo "${ADDITIONAL_MOUNTPOINT[$i]}"
+						mkdir -p "${ADDITIONAL_MOUNTPOINT[$i]}"
+						echo "${ADDITIONAL_DRIVES[$i]}${ADDITIONAL_PARTITION[$i]}"
+						mount "${ADDITIONAL_DRIVES[$i]}${ADDITIONAL_PARTITION[$i]}" "${ADDITIONAL_MOUNTPOINT[$i]}"
+					fi
+
+					currentPartition=$((currentPartition + 1))
+				fi
+			done
+
 			if [[ "$IS_UEFI" = "true" && "$EFI_DRIVE" = "$drive" && "$EFI_PARTITION" -eq $currentPartition ]]; then
 				parted -s "$drive" mkpart "$EFI_NAME" fat32 "${EFI_SIZE[0]}" "${EFI_SIZE[1]}"
 				parted -s "$drive" set "$EFI_PARTITION" esp on
+
+				mkfs.fat -F 32 -n "$EFI_LABEL" "${EFI_DRIVE}${EFI_PARTITION}"
+				mkdir -p /mnt/efi
+				mount "${EFI_DRIVE}${EFI_PARTITION}" /mnt/efi
+
 				currentPartition=$((currentPartition + 1))
 			fi
 
@@ -173,27 +242,12 @@ function partition_drives() {
 					parted -s "$drive" set "$BOOT_PARTITION" boot on
 				fi
 
+				mkfs.ext4 -L "$BOOT_LABEL" "${BOOT_DRIVE}${BOOT_PARTITION}"
+
 				currentPartition=$((currentPartition + 1))
 			fi
 		done
 	done
-
-	mkswap -L $SWAP_LABEL "${SWAP_DRIVE}${SWAP_PARTITION}"
-	swapon "${SWAP_DRIVE}${SWAP_PARTITION}"
-
-	if [ "$BOOT_DRIVE" != "root" ]; then
-		echo "TESTING"
-		mkfs.ext4 -L "$BOOT_LABEL" "${BOOT_DRIVE}${BOOT_PARTITION}"
-		mkdir -p /mnt/boot
-		mount "${BOOT_DRIVE}${BOOT_PARTITION}" /mnt/boot
-		echo "TESTING"
-	fi
-
-	if [ "$IS_UEFI" = "true" ]; then
-		mkfs.fat -F 32 -n "$EFI_LABEL" "${EFI_DRIVE}${EFI_PARTITION}"
-		mkdir -p /mnt/efi
-		mount "${EFI_DRIVE}${EFI_PARTITION}" /mnt/efi
-	fi
 }
 
 function pre_checks() {
@@ -220,8 +274,19 @@ function pre_checks() {
 }
 
 function os_installation() {
+	if [ "$BOOT_DRIVE" != "root" ]; then
+		mkdir -p /mnt/boot
+		mount "${BOOT_DRIVE}${BOOT_PARTITION}" /mnt/boot
+	fi
+
 	pacstrap /mnt "${PackagesNeeded[@]}" > archlinux-install_pacstrap.log 2>&1
+	umount /mnt/boot
 	genfstab -U /mnt >> /mnt/etc/fstab
+	if [ "$BOOT_DRIVE" != "root" ]; then
+		mkdir -p /mnt/boot
+		mount "${BOOT_DRIVE}${BOOT_PARTITION}" /mnt/boot
+	fi
+	
 	arch-chroot /mnt ln -sf /usr/share/zoneinfo/$TIMEZONE_REGION/$TIMEZONE_CITY /etc/localtime
 	arch-chroot /mnt hwclock --systohc
 
@@ -232,13 +297,15 @@ function os_installation() {
 	echo "$HOSTNAME" > /mnt/etc/hostname
 	printf '127.0.0.1\tlocalhost\n::1\tlocalhost\n127.0.1.1\t%s\n' "$HOSTNAME" > /mnt/etc/hosts
 
+	# TODO: Make sure this is correct
 	if [ "$GPU" = "nvidia" ]; then
 		sed -i "s/MODULES=()/MODULES=(nvidia nvidia_modeset nvidia_uvm nvidia_drm)/" /mnt/etc/mkinitcpio.conf
 		sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"%GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet nvidia-drm.modeset=1\"%"
 		printf "[Trigger]\nOperation=Install\nOperation=Upgrade\nOperation=Remove\nType=Package\nTarget=nvidia\nTarget=linux\n# Change the linux part above and in the Exec line if a different kernel is used\n\n[Action]\nDescription=Update Nvidia module in initcpio\nDepends=mkinitcpio\nWhen=PostTransaction\nNeedsTargets\nExec=/bin/sh -c 'while read -r trg; do case \$trg in linux) exit 0; esac; done; /usr/bin/mkinitcpio -P'\n" > /mnt/etc/pacman.d/hooks/nvidia.hook
 	fi
 
-	if [ $ENCRYPT = "y" ]; then
+	# TODO: Correct this
+	if [ "$ENCRYPT" = "y" ]; then
 		sed -i "s%GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet\"%GRUB_CMDLINE_LINUX_DEFAULT=\"loglevel=3 quiet cryptdevice=UUID=$(lsblk -dno UUID ${ROOT_DRIVE}${ROOT_PARTITION}):root root=/dev/mapper/root\"%" /mnt/etc/default/grub
 		sed -i "s/HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect keyboard modconf block encrypt filesystems keyboard fsck)/" /mnt/etc/mkinitcpio.conf	
 		arch-chroot /mnt mkinitcpio -P
@@ -246,7 +313,6 @@ function os_installation() {
 
 	echo "Enter the root password"
 	arch-chroot /mnt passwd
-	arch-chroot /mnt SUDO_EDITOR='sed -i "s/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL"' visudo
 
 	if [ "$IS_UEFI" = "true" ]; then
 		arch-chroot /mnt grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB

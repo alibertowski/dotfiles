cae3a4439a629bb9293ce7050fc0e966413f4d28
Alex Libertowski <57733758+alibertowski@users.noreply.github.com>
2025-12-19 08:37:03 -0500

    Updated Neovim configurations

--- configurations/program-configs/nvim/init.lua
+++ configurations/program-configs/nvim/init.lua
@@ -11,8 +11,8 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_node_provider = 0
 
 -- Enable useful options
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.g.mapleader = " "
@@ -129,3 +129,27 @@ local capabilities = require('cmp_nvim_lsp').default_capabilities()
 vim.lsp.config('*', {
 	capabilities = capabilities
 })
+
+local cmp = require('cmp')
+cmp.setup({
+		sources = {
+			{ name = 'nvim_lsp' },
+			{ name = 'luasnip' },
+		},
+		snippet = {
+			expand = function(args)
+				require('luasnip').lsp_expand(args.body)
+			end
+		},
+		window = {
+			completion = cmp.config.window.bordered(),
+      			documentation = cmp.config.window.bordered(),
+    		},
+		mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.select_prev_item(),
+      ['<C-f>'] = cmp.mapping.select_next_item(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+	})
+
--- configurations/program-configs/nvim/lua/plugins/nvim-cmp.lua
+++ configurations/program-configs/nvim/lua/plugins/nvim-cmp.lua
@@ -4,16 +4,5 @@ return {
 		"hrsh7th/cmp-nvim-lsp",
 		"L3MON4D3/LuaSnip",
 		"saadparwaiz1/cmp_luasnip"
-	},
-	opts = {
-		sources = {
-			{ name = 'nvim_lsp' },
-			{ name = 'luasnip' },
-		},
-		snippet = {
-			expand = function(args)
-				require('luasnip').lsp_expand(args.body)
-			end
-		}
 	}
 }

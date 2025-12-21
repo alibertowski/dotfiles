f70b47d38c51b2cb56bf2da5c88b1ad0d45f1f5b
Alex Libertowski <57733758+alibertowski@users.noreply.github.com>
2025-12-21 17:18:05 -0500

    Removed wireguard scripts

--- scripts/useful/disable-wireguard.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mv /etc/systemd/network/50-wg0.network /etc/systemd/network/99-wg0.netdev ~/
-systemctl restart systemd-networkd
-ufw default allow outgoing
--- scripts/useful/start-wireguard.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-mv ~/50-wg0.network ~/99-wg0.netdev /etc/systemd/network
-systemctl restart systemd-networkd
-ufw default deny outgoing
